aeb318a3a50899f9785ea2698d9ccb95207ec048
Gautier Berthou <gautier.berthou@insa-lyon.fr>
2015-04-27 22:43:03 +0200

    [Server] Astar implementation - not tested

--- server/pathfinder.h
+++ server/pathfinder.h
@@ -1,7 +1,13 @@
 #ifndef PATHFINDER_H
 #define PATHFINDER_H
 
+#include <vector>
+#include <deque>
+
 #include "general.h"
+
+// start and goal must exist in the nodes array
+bool PF_Astar(PathNode *start, PathNode *goal, const std::vector<PathNode*> &nodes, double (*distance)(const PathNode*, const PathNode*), double (*heuristic)(const PathNode*, const PathNode*), std::deque<PathNode*> &result);
 
 void TestPathfinder(void);
 
