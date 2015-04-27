64e6fb98f056e01afac599f296993203de545f5a
Gautier Berthou <gautier.berthou@insa-lyon.fr>
2015-04-27 16:06:53 +0200

    [Server] Added pathfinder

---
+++ server/pathfinder.cpp
@@ -0,0 +1,15 @@
+#include "pathfinder.h"
+
+void TestPathfinder(void)
+{
+	const unsigned int W = 4;
+	const unsigned int H = 4;
+
+	Coordinates coord[W];
+
+	for(unsigned int j = 0; j < H; ++j)
+		for(unsigned int i = 0; i < W; ++i)
+		{
+		}
+}
+
---
+++ server/pathfinder.h
@@ -0,0 +1,29 @@
+#ifndef PATHFINDER_H
+#define PATHFINDER_H
+
+enum RoadDirection
+{
+	RD_BOTH_DIRECTIONS,
+	RD_1_TO_2,
+	RD_2_TO_1
+};
+
+struct Coordinates
+{
+	double longitude;
+	double latitude;
+};
+
+struct Road
+{
+	double distance;
+	Coordinates *points; // Used only to send the road shape to the client
+	unsigned int node1; // Index
+	unsigned int node2; // Index
+	RoadDirection direction;
+};
+
+void TestPathfinder(void);
+
+#endif
+
