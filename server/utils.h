92289c6437becff76f1187cee12b44315ced9747
Gautier Berthou <gautier.berthou@insa-lyon.fr>
2015-04-29 09:38:06 +0200

    [Server] Added parsing functions

--- server/utils.h
+++ server/utils.h
@@ -1,6 +1,7 @@
 #ifndef UTILITIES_H
 #define UTILITIES_H
 
+#include <string>
 #include <map>
 
 #include "general.h"
@@ -8,6 +9,9 @@
 bool operator<(const Coordinates &a, const Coordinates &b);
 
 void error(const char* msg);
+
+double ParseDouble(const std::string &s);
+unsigned int ParseUint(const std::string &s);
 
 PathNode *GetClosestNode(const std::map<Coordinates, PathNode*> &nodes, const Coordinates &coord);
 
