d36bc06e6adddbd49a4c07ecc2a8a4c1a63ecae7
slallement <serguei.l@orange.fr>
2015-04-29 11:42:11 +0200

    update index.js + socket.io deleted

--- client/pokelyon/www/js/index.js
+++ client/pokelyon/www/js/index.js
@@ -44,19 +44,15 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+		var buf = new ArrayBuffer(4+4*4);
+		var type_req = new Int16Array(buf,0,1);
+		var type_junk = new Int16Array(buf,2,1);
+		var gpscoord = new Float32Array(buf,4,4);
+		
+		type_req = 0;type_junk = 42;
 
-        chrome.socket.create('tcp', {}, function(createInfo) {
-          chrome.socket.connect(createInfo.socketId, "192.168.1.199", 6666, function(result) {
-            alert("prout");
-            chrome.socket.write(createInfo.socketId, str2ab("lauwlnkxhkldhqkjhdkl<kjhqklsik"), function() {
-              alert("ouiiiii");
-              chrome.socket.disconnect(createInfo.socketId);
-            });
-          });
-        });
-
-
-        var onSuccess = function(position) {
+		
+		var onSuccess = function(position) {
             alert('Latitude: '          + position.coords.latitude          + '\n' +
                   'Longitude: '         + position.coords.longitude         + '\n' +
                   'Altitude: '          + position.coords.altitude          + '\n' +
@@ -65,6 +61,10 @@ var app = {
                   'Heading: '           + position.coords.heading           + '\n' +
                   'Speed: '             + position.coords.speed             + '\n' +
                   'Timestamp: '         + position.timestamp                + '\n');
+			gpscoord[0] = position.coords.latitude;
+			gpscoord[1] = position.coords.longitude; 
+			gpscoord[2] = position.coords.latitude;
+			gpscoord[3] = position.coords.longitude; 
         };
 
         // onError Callback receives a PositionError object
@@ -75,6 +75,21 @@ var app = {
         }
 
         navigator.geolocation.getCurrentPosition(onSuccess, onError);
+		
+		
+		
+        chrome.socket.create('tcp', {}, function(createInfo) {
+          chrome.socket.connect(createInfo.socketId, "192.168.1.199", 6666, function(result) {
+            alert("prout");
+            chrome.socket.write(createInfo.socketId, buf, function() {
+              alert("ouiiiii");
+              chrome.socket.disconnect(createInfo.socketId);
+            });
+          });
+        });
+
+
+
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
