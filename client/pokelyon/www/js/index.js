70ceee06b60ac5fb8148eb88adfc97cf823c768f
slallement <serguei.l@orange.fr>
2015-04-30 15:39:12 +0200

    indent power

--- client/pokelyon/www/js/index.js
+++ client/pokelyon/www/js/index.js
@@ -1,6 +1,6 @@
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
+ * or more contributor license agreements.	See the NOTICE file
  * distributed with this work for additional information
  * regarding copyright ownership.  The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
@@ -12,122 +12,128 @@
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
+ * KIND, either express or implied.	 See the License for the
  * specific language governing permissions and limitations
  * under the License.
  */
 
-function str2ab(str) {
-  var buf = new ArrayBuffer(str.length);
-  var bufView = new Uint8Array(buf);
-  for (var i=0, strLen=str.length; i<strLen; i++) {
-    bufView[i] = str.charCodeAt(i);
-  }
-  return buf;
+ function str2ab(str) {
+	var buf = new ArrayBuffer(str.length);
+	var bufView = new Uint8Array(buf);
+	for (var i=0, strLen=str.length; i<strLen; i++) {
+		bufView[i] = str.charCodeAt(i);
+	}
+	return buf;
 }
 
 function arrayBufferToString(buffer) {
-    return String.fromCharCode.apply(null, new Uint8Array(buffer));
+	return String.fromCharCode.apply(null, new Uint8Array(buffer));
 }
 
 function sendQuery(buf) {
-    chrome.socket.create('tcp', {}, function(createInfo) {
-      //alert(createInfo);
-      var socketId = createInfo.socketId;
-      chrome.socket.connect(socketId, "192.168.1.199", 6666, function(result) {
-        if(result >= 0) {
-          alert("prout " + result);
-          chrome.socket.write(socketId, buf, function(writeInfo) {
-            if(writeInfo.bytesWritten > 0) {
-              alert("ouiiiii ");
-              chrome.socket.read(socketId, 1000, function(readInfo) {
-                alert(arrayBufferToString(readInfo.data));
-        				var path = parseData(readInfo.data);
-                alert(path);
-                alert(Map.map);
-        				drawPathOnMap(Map.map,path);
-              });
-              alert("TRALALALALALA");
-              //chrome.socket.disconnect(socketId);
-            } else {
-              // Cannot send data
-              // Do something smart 
-            }
-          });
-        } else {
-          // Cannot connect
-          // Do something smart
-        }
+	chrome.socket.create('tcp', {}, function(createInfo) {
+		//alert(createInfo);
+		var socketId = createInfo.socketId;
+		chrome.socket.connect(socketId, "192.168.1.199", 6666, function(result) {
+			if(result >= 0) {
+				alert("prout " + result);
+				chrome.socket.write(socketId, buf, function(writeInfo) {
+					if(writeInfo.bytesWritten > 0) {
+						alert("ouiiiii ");
+						chrome.socket.read(socketId, 1000, function(readInfo) {
+							//alert(arrayBufferToString(readInfo.data));
+							var path = parseData(readInfo.data);
+							var str = "";
+							for(var i=0;i<r.length;i++){
+								if(r[i] !== undefined){
+									str += path[i].x+" "+path[i].y+"\n";
+								}
+							}
 
-      });
-    });
+							alert(str);
+							drawPathOnMap(Map.map,path);
+						});
+						//alert("TRALALALALALA");
+						//chrome.socket.disconnect(socketId);
+					} else {
+					// Cannot send data
+					 // Do something smart 
+					}
+				});
+			} else {
+			  // Cannot connect
+			  // Do something smart
+			}
+
+		});
+	});
 }
 
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    		var buf = new ArrayBuffer(8+4*8);
-    		var type_req = new Int16Array(buf,0,1);
-    		var type_junk = new Int16Array(buf,2,1);
-        var type_junk2 = new Int32Array(buf,4,1);
-    		var gpscoord = new Float64Array(buf,8,4);
-    		
-    		type_req[0] = 0;
-        type_junk[0] = 42;
-        type_junk2[0] = 42;
+	// Application Constructor
+	initialize: function() {
+	  this.bindEvents();
+  },
+	// Bind Event Listeners
+	//
+	// Bind any events that are required on startup. Common events are:
+	// 'load', 'deviceready', 'offline', and 'online'.
+	bindEvents: function() {
+	  document.addEventListener('deviceready', this.onDeviceReady, false);
+  },
+	// deviceready Event Handler
+	//
+	// The scope of 'this' is the event. In order to call the 'receivedEvent'
+	// function, we must explicitly call 'app.receivedEvent(...);'
+	onDeviceReady: function() {
+	  app.receivedEvent('deviceready');
+	  var buf = new ArrayBuffer(8+4*8);
+	  var type_req = new Int16Array(buf,0,1);
+	  var type_junk = new Int16Array(buf,2,1);
+	  var type_junk2 = new Int32Array(buf,4,1);
+	  var gpscoord = new Float64Array(buf,8,4);
 
-    		var onSuccess = function(position) {
-            /*alert('Latitude: '              + position.coords.latitude          + '\n' +
-                      'Longitude: '         + position.coords.longitude         + '\n' +
-                      'Altitude: '          + position.coords.altitude          + '\n' +
-                      'Accuracy: '          + position.coords.accuracy          + '\n' +
-                      'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-                      'Heading: '           + position.coords.heading           + '\n' +
-                      'Speed: '             + position.coords.speed             + '\n' +
-                      'Timestamp: '         + position.timestamp                + '\n');*/
-      			gpscoord[0] = position.coords.latitude;
-      			gpscoord[1] = position.coords.longitude; 
-      			gpscoord[2] = 45.6803042752;
-      			gpscoord[3] = 4.92207816575;
-            //alert(gpscoord[1]);
-            sendQuery(buf);
-        };
+	  type_req[0] = 0;
+	  type_junk[0] = 42;
+	  type_junk2[0] = 42;
 
-        // onError Callback receives a PositionError object
-        //
-        function onError(error) {
-            alert('code: '    + error.code    + '\n' +
-                  'message: ' + error.message + '\n');
-        }
+	  var onSuccess = function(position) {
+			/*alert('Latitude: '			  + position.coords.latitude		  + '\n' +
+					  'Longitude: '			+ position.coords.longitude			+ '\n' +
+					  'Altitude: '			+ position.coords.altitude			+ '\n' +
+					  'Accuracy: '			+ position.coords.accuracy			+ '\n' +
+					  'Altitude Accuracy: ' + position.coords.altitudeAccuracy	+ '\n' +
+					  'Heading: '			+ position.coords.heading			+ '\n' +
+					  'Speed: '				+ position.coords.speed				+ '\n' +
+					  'Timestamp: '			+ position.timestamp				+ '\n');*/
+gpscoord[0] = position.coords.latitude;
+gpscoord[1] = position.coords.longitude; 
+gpscoord[2] = 45.6803042752;
+gpscoord[3] = 4.92207816575;
+			//alert(gpscoord[1]);
+			sendQuery(buf);
+		};
 
-        navigator.geolocation.getCurrentPosition(onSuccess, onError);
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        /*var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+		// onError Callback receives a PositionError object
+		//
+		function onError(error) {
+		  alert('code: '	+ error.code	+ '\n' +
+			'message: ' + error.message + '\n');
+	  }
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');*/
+	  navigator.geolocation.getCurrentPosition(onSuccess, onError);
+  },
+	// Update DOM on a Received Event
+	receivedEvent: function(id) {
+		/*var parentElement = document.getElementById(id);
+		var listeningElement = parentElement.querySelector('.listening');
+		var receivedElement = parentElement.querySelector('.received');
 
-        console.log('Received Event: ' + id);
-    }
+		listeningElement.setAttribute('style', 'display:none;');
+		receivedElement.setAttribute('style', 'display:block;');*/
+
+		console.log('Received Event: ' + id);
+	}
 };
 
 app.initialize();
--- client/pokelyon/www/js/utils.js
+++ client/pokelyon/www/js/utils.js
@@ -8,6 +8,7 @@
 function parseData(buffer) {
 	var sizeview = new Uint32Array(buffer);
 	var size = sizeview[1];
+	alert(size);
 	buffer = buffer.slice(8);
 	var sizeview = new Float64Array(buffer);
 	var path = [];
@@ -26,12 +27,15 @@ function parseData(buffer) {
 */
 function drawPathOnMap(map, path){
 	var pointList = [];
-	for(var i=0;i<r.length;++i){
-		pointList.push([r[i].x,r[i].y]);
+	for(var i=0;i<path.length;++i){
+		if(path[i] !== undefined){
+			pointList.push([path[i].x,path[i].y]);
+		}
 	}
 	
-	L.marker([r[0].x,r[0].y]).addTo(map);
-	L.marker([r[r.length-1].x,r[r.length-1].y]).addTo(map);
+	L.marker([path[0].x,path[0].y]).addTo(map);
+	/*if(path[path.length-1] !== undefined)
+		L.marker([path[path.length-1].x,path[path.length-1].y]).addTo(map);*/
 	
 	var polyline = L.polyline(pointList, {color: 'blue',
 		opacity: 0.9,
