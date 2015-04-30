91b119ad174739bd250859dcee5c521d3ed69d7f
slallement <serguei.l@orange.fr>
2015-04-30 10:54:39 +0200

    update of utils

--- client/pokelyon/www/js/utils.js
+++ client/pokelyon/www/js/utils.js
@@ -7,8 +7,7 @@
 */
 function parseData(buffer) {
 	var sizeview = new Uint32Array(buffer);
-	var size = sizeview[0];
-	
+	var size = sizeview[1];
 	buffer = buffer.slice(8);
 	var sizeview = new Float64Array(buffer);
 	var path = [];
@@ -18,4 +17,23 @@ function parseData(buffer) {
 	}
 	
 	return path;
+}
+
+
+/* usage :
+	var path = parseData(buffer);
+	drawPathOnMap(map, path);
+*/
+function drawPathOnMap(map, path){
+	var pointList = [];
+	for(var i=0;i<r.length;++i){
+		pointList.push([r[i].x,r[i].y]);
+	}
+	
+	L.marker([r[0].x,r[0].y]).addTo(map);
+	L.marker([r[r.length-1].x,r[r.length-1].y]).addTo(map);
+	
+	var polyline = L.polyline(pointList, {color: 'blue',
+		opacity: 0.9,
+		smoothFactor: 1 }).addTo(map);
 }
