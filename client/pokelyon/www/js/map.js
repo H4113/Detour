e330d50cd5147b28bccd7ac1eda5a654f6063ffc
Robin Nicolet <robsnicolet@hotmail.fr>
2015-05-01 20:17:32 +0200

    app actually locates itself...

--- client/pokelyon/www/js/map.js
+++ client/pokelyon/www/js/map.js
@@ -1,6 +1,8 @@
 var Map = {
-	create: function() {
-		this.map = L.map('map').setView([45.757927, 4.847598], 15);
+	create: function(options) {
+
+		this.map = L.map('map').setView([45.757927, 4.847598],15);
+		this.map.locate({setView: true, maxZoom:16});
 
 		this.layer = L.tileLayer('https://{s}.tiles.mapbox.com/v3/examples.map-i875mjb7/{z}/{x}/{y}.png', {
 			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -20,6 +22,7 @@ var Map = {
 };
 
 document.addEventListener('deviceready', function(e) {
+	
 	Map.create();
 
 	Map.onClick(function(click){
