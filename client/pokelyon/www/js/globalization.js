ae8ac9254dcfca5261f4aa6c476b1ce9941e7e03
francois-caddet <francois.caddet@icloud.com>
2015-05-06 09:18:35 +0200

    automaticaly set the locale

--- client/pokelyon/www/js/globalization.js
+++ client/pokelyon/www/js/globalization.js
@@ -1,4 +1,4 @@
-var Locale = "de";
+var Locale = "en";
 var textLang = $(".lang");
 var imgLang = $("img");
 
@@ -10,26 +10,47 @@ var Lang = {
 	}
 };
 
-$.getJSON("lang.json", function(lang)
+document.addEventListener("deviceready", initLocale, false);
+
+function initLocale() {
+	navigator.globalization.getPreferredLanguage(
+	    function (language) {
+			Locale =language.value.split("-")[0];
+			successLocale();
+		},
+	    function () {errorLocale();}
+	);
+}
+
+function successLocale() 
 {
-
-	Lang.data = lang;
-
-	for (var i = 0; i < textLang.length; i++) 
+	alert ("language: " + Locale);
+	$.getJSON("lang.json", function(lang)
 	{
-		if (textLang[i].hasAttribute("data-lang"))
-		{
-			textLang[i].textContent = lang[textLang[i].getAttribute("data-lang")][Locale];
-		}
-	}
-	for (var i = 0; i < imgLang.length; i++)
-	{
-		if (imgLang[i].hasAttribute("alt"))
-		{
-			imgLang[i].setAttribute("alt", lang[imgLang[i].getAttribute("alt")][Locale]);
-		}
-	}
 
-	//document.dispatchEvent()
-});
+		Lang.data = lang;
 
+		for (var i = 0; i < textLang.length; i++) 
+		{
+			if (textLang[i].hasAttribute("data-lang"))
+			{
+				textLang[i].textContent = lang[textLang[i].getAttribute("data-lang")][Locale];
+			}
+		}
+		for (var i = 0; i < imgLang.length; i++)
+		{
+			if (imgLang[i].hasAttribute("alt"))
+			{
+				imgLang[i].setAttribute("alt", lang[imgLang[i].getAttribute("alt")][Locale]);
+			}
+		}
+
+		//document.dispatchEvent()
+	});
+}
+
+function errorLocale() 
+{
+	alert ("Locale error");
+	Locale = "en";
+}
